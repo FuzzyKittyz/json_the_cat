a960e6a82d3ca872df5462de3500b29f125770f4
FuzzyKittyz <topher.a@hotmail.com>
2022-09-07 15:52:46 -0600

    Checks the description of a breed using an api

---
+++ breedFetcher.js
@@ -0,0 +1,14 @@
+const request = require('request');
+
+const cl = process.argv[2];
+const key = cl.slice(0, 4).toLowerCase();
+
+request(`https://api.thecatapi.com/v1/breeds/${key}`, (error, response, body) => {
+  if (body === "{}") {
+    console.log('Breed is not found');
+    return;
+  }
+  console.log('error:', error);
+  const data = JSON.parse(body);
+  console.log(data.description);
+});
